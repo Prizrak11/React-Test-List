378b8277d965423dc8c3ca54b6e35845aeaa6af5
Santiago Patiño <santipato2004@gmail.com>
2022-02-10 09:18:36 -0800

    update reducer to builder notation

--- src/features/napster/napsterSlice.js
+++ src/features/napster/napsterSlice.js
@@ -10,24 +10,43 @@ const initialState = {
   }
 }
 
+// {
+//   [initializePlaylists.pending]: (state) => {
+//     state.isLoading = true
+//   },
+//     [initializePlaylists.fulfilled]: (state, { payload }) => {
+//       state.playlists = payload
+//       state.isLoading = false
+//     },
+//       [changeCurrentPlaylist.pending]: (state) => {
+//         state.isLoading = true
+//       },
+//         [changeCurrentPlaylist.fulfilled]: (state, { payload }) => {
+//           state.currentPlaylist = payload
+//           state.isLoading = false
+//         }
+// }
+
 export const napsterSlice = createSlice({
   name: '@napster',
   initialState,
-  extraReducers: {
-    [initializePlaylists.pending]: (state) => {
-      state.isLoading = true
-    },
-    [initializePlaylists.fulfilled]: (state, { payload }) => {
-      state.playlists = payload
-      state.isLoading = false
-    },
-    [changeCurrentPlaylist.pending]: (state) => {
-      state.isLoading = true
-    },
-    [changeCurrentPlaylist.fulfilled]: (state, { payload }) => {
-      state.currentPlaylist = payload
-      state.isLoading = false
-    }
+  extraReducers: builder => {
+    console.log(builder)
+    builder
+      .addCase(initializePlaylists.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(initializePlaylists.fulfilled, (state, { payload }) => {
+        state.playlists = payload
+        state.isLoading = false
+      })
+      .addCase(changeCurrentPlaylist.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(changeCurrentPlaylist.fulfilled, (state, { payload }) => {
+        state.currentPlaylist = payload
+        state.isLoading = false
+      })
   }
 })
 
